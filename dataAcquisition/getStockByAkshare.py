57929d3e7921a8f1320c0cd476cbfd9c2f488701
paco <tanbao210@hotmail.com>
2024-02-05 17:55:42 +0800

    add new directory structure

---
+++ dataAcquisition/getStockByAkshare.py
@@ -0,0 +1,13 @@
+import os
+import time
+import warnings
+from datetime import timedelta, datetime
+import akshare as ak
+import pandas as pd
+import numpy as np
+
+
+# ##############################################打印版本号
+print(f'pandas version: {pd.__version__}')
+print(f'akshare version: {ak.__version__}')
+
